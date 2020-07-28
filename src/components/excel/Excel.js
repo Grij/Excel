f66837891b11b95131dabd873e7463d7be09bd11
Grij <52110638+Grij@users.noreply.github.com>
2020-07-28 15:04:11 +0300

    SUKA don't WORK! BLT

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,4 +3,20 @@ export class Excel {
     this.$el = document.querySelector(selector)
     this.components = options.components || []
   }
+
+  getRoot() {
+    const $root = document.createElement('div')
+// ПРоитерировали компоненты с большой буквы и получили екземпляры компонентов КРУТЬ!
+    this.components.forEach(Component => {
+      const component = new Component()
+      // afterbegin, afterend, beforender, beforebegin
+      $root.insertAdjacentHTML('beforeend', component.toHTML())
+    })
+
+    return $root
+  }
+
+  render() {
+    this.$el.append(this.getRoot())
+  }
 }
